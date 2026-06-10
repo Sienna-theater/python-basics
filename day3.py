324965c07f8abe61234522940a6149d2eaf097ed
Sienna Clifford <siennaclifford2013@gmail.com>
2026-06-10 16:23:18 -0500

    Adding my first few days of python programming

---
+++ day3.py
@@ -0,0 +1,47 @@
+temputure = 19
+
+if temputure > 25:
+    print("Its hot outside!")
+score = 82
+
+if score >=90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("Keep practising!")
+
+
+age = 1
+has_ticket = True
+
+if age >= 13 and has_ticket:
+    print("Welcome to the show!")
+
+if age < 5 or age > 65:
+    print("Free entry!")
+
+if not has_ticket:
+    print("You need a ticket to enter.")
+
+
+
+    
+age = 47
+
+if age < 13:
+    print("You are a kid")
+
+if age >13 or age <=17:
+    print("You are a teenager")
+
+if age >= 18 and age <40:
+    print("You are an adult")
+
+if age >40 and age <60:
+    print("you are most likly having a midlife crisis")
+
+if age >60:
+    print("your so old even your dentures are decaying")
